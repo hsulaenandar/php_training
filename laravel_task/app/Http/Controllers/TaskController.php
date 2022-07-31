84dd84691c636b687fc6f751deeaa63f75d25e7a
hsulaenandar <hsulaenandar1998@gmail.com>
2022-07-31 22:31:09 +0630

    laravel_basic_task_list

---
+++ laravel_task/app/Http/Controllers/TaskController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+ 
+        return view('tasks', [
+            'tasks' => $tasks
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+       
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+     
+        if ($validator->fails()) {
+            return redirect('/')
+                ->withInput()
+                ->withErrors($validator);
+        }
+     
+        $task = new Task;
+        $task->name = $request->name;
+        $task->save();
+     
+        return redirect('task');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $tasks = Task::orderBy('created_at', 'asc')->get();
+        $task = Task::findOrFail($id);
+        return view('tasks', [
+            'tasks' => $tasks,
+            'task' => $task
+        ]);
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        echo "Hey";
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:255',
+        ]);
+     
+        if ($validator->fails()) {
+            return redirect('task')
+                ->withInput()
+                ->withErrors($validator);
+        }
+     
+        $task = Task::findOrFail($id);
+        $task->name = $request->name;
+        $task->save();
+     
+        return redirect('task');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Task::findOrFail($id)->delete();
+ 
+        return redirect('task');
+    }
+}
---
+++ laravel_task/resources/views/tasks.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+ 
+@section('content')
+ 
+    <!-- Bootstrap Boilerplate... -->
+ 
+    <div class="panel-body">
+        <!-- Display Validation Errors -->
+
+        <!-- @include('common.errors') -->
+ 
+        <!-- New Task Form -->
+
+        @if(isset($task))
+        <form method="post" action="{{route('task.update', $task->id ?? '')}}">
+            @method('PUT')
+        @else
+
+        <form action="task" method="POST" class="form-horizontal">
+        
+        @endif
+            {{ csrf_field() }}
+ 
+            
+            <!-- Task Name -->
+            <div class="form-group">
+                <label for="task" class="col-sm-3 control-label">Task</label>
+ 
+                <div class="col-sm-6">
+                    <input type="text" name="name" value="{{ old('task', isset($task) ? $task->name: '') }}" id="task-name" class="form-control">
+                </div>
+            </div>
+ 
+            <!-- Add Task Button -->
+            <div class="form-group">
+                <div class="col-sm-offset-3 col-sm-6">
+                    <button type="submit" class="btn btn-default">
+                        <i class="fa fa-plus"></i> Add Task
+                    </button>
+                </div>
+            </div>
+        </form>
+    </div>
+ 
+    <!-- TODO: Current Tasks -->
+
+    <!-- Current Tasks -->
+    @if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+ 
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+ 
+                    <!-- Table Headings -->
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+ 
+                    <!-- Table Body -->
+                    <tbody>
+                        @foreach ($tasks as $task)
+                        <tr>
+                            <!-- Task Name -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
+
+                            <td>
+                                <form action="/task/{{ $task->id }}/edit" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('GET') }}
+                        
+                                    <button>Edit Task</button>
+                                </form>
+                            </td>
+
+                            <!-- Delete Button -->
+                            <td>
+                                <form action="/task/{{ $task->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                        
+                                    <button>Delete Task</button>
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
+    
+@endsection
